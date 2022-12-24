97a3dbb9ca2bd7fa1ec3d890cb25a02df3938587
ittikorn chawkamud <aseaways1234@gmail.com>
2022-12-24 17:51:37 +0700

    input 3 should return Fizz

--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -15,7 +15,18 @@ func TestFizzBuzz(t *testing.T) {
 func TestInput2ShouldReturn2FizzBuzz(t *testing.T) {
 	want := "2"
 	input := 2
-	got := FizzBuzz(2)
+
+	got := FizzBuzz(input)
+
+	if got != want {
+		t.Errorf("FizzBuzz(%d) = %q, want %q", input, got, want)
+	}
+}
+
+func TestInput3ShouldReturn2FizzBuzz(t *testing.T) {
+	want := "Fizz"
+	input := 3
+	got := FizzBuzz(input)
 
 	if got != want {
 		t.Errorf("FizzBuzz(%d) = %q, want %q", input, got, want)
