0134cebe74ea5f3a57058e32e0ad618437ef7fb2
ittikorn chawkamud <aseaways1234@gmail.com>
2022-12-26 21:43:47 +0700

    push dependency

--- rental/customer.go
+++ rental/customer.go
@@ -45,14 +45,29 @@ func getPoint(r Rental) int {
 	return 1
 }
 
-func (rcvr Customer) Statement() string {
-	totalAmount := 0.0
-	frequentRenterPoints := 0
+func getTotalAmount(rental []Rental) float64 {
+	result := 0.0
+	for _, r := range rental {
+		result += r.Charge()
+	}
+	return result
+}
 
-	result := fmt.Sprintf("Rental Record for %v\n", rcvr.Name())
-	for _, r := range rcvr.rentals {
-		frequentRenterPoints += getPoint(r)
-		totalAmount += r.Charge()
+func getTotalPoint(rentals []Rental) int {
+	result := 0
+	for _, r := range rentals {
+		result += getPoint(r)
+	}
+
+	return result
+}
+
+func (c Customer) Statement() string {
+	frequentRenterPoints := getTotalPoint(c.rentals)
+	totalAmount := getTotalAmount(c.rentals)
+
+	result := fmt.Sprintf("Rental Record for %v\n", c.Name())
+	for _, r := range c.rentals {
 		result += fmt.Sprintf("\t%v\t%.1f\n", r.Movie().Title(), r.Charge())
 	}
 	result += fmt.Sprintf("Amount owed is %.1f\n", totalAmount)
